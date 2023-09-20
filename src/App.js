293a99c905a555929a6a131d546b89b1b8dd4924
JMGino <jmgino909@gmail.com>
2023-09-19 18:04:42 -0600

    Added project skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-function App() {
+import About from './components/About';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Nav from './components/Nav';
+import Services from './components/Services';
+import Tours from './components/Tours';
+
+const App = () => {
   return (
     <div>
-
+      <Nav />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
